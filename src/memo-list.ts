5082d48d526acb1d8d31a4958c713fa513643219
gngsn <rudtjs4540@gmail.com>
2019-03-18 17:44:41 +0900

    update CSS

--- src/memo-list.ts
+++ src/memo-list.ts
@@ -5,5 +5,15 @@ export const MEMOS: Memo[] = [
   { userName: 'gngsn', content: 'Hi, my name is gngsn', created: Date.now() },
   { userName: 'Jinsuu', content: 'Im so tired', created: Date.now() },
   { userName: 'Doctor Who', content: 'doctor....who!', created: Date.now() },
+  { userName: 'sherlock', content: 'think, think,think...', created: Date.now() },
+  { userName: 'Anne Marie', content: 'Hi, my name is Marie', created: Date.now() },
+  { userName: 'gngsn', content: 'Hi, my name is gngsn', created: Date.now() },
+  { userName: 'Jinsuu', content: 'Im so tired', created: Date.now() },
+  { userName: 'Doctor Who', content: 'doctor....who!', created: Date.now() },
+  { userName: 'sherlock', content: 'think, think,think...', created: Date.now() },
+  { userName: 'Anne Marie', content: 'Hi, my name is Marie', created: Date.now() },
+  { userName: 'gngsn', content: 'Hi, my name is gngsn', created: Date.now() },
+  { userName: 'Jinsuu', content: 'Im so tired', created: Date.now() },
+  { userName: 'Doctor Who', content: 'doctor....who!', created: Date.now() },
   { userName: 'sherlock', content: 'think, think,think...', created: Date.now() }
 ];
