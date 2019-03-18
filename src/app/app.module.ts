221724731b3bccec468fbaff5f26d8e138601a8f
tomas4540 <rudtjs4540@gmail.com>
2019-03-18 21:54:57 +0900

    create form component, services and modify memo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent
+    BoardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
