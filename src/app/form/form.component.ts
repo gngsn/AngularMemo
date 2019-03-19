ae55b61cb33092612ee05d8ffd1cac5284a23fe0
gngsn <rudtjs4540@gmail.com>
2019-03-19 15:48:30 +0900

    correct an error

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -14,11 +14,10 @@ export class FormComponent implements OnInit {
   ngOnInit() {
   }
   onCreate(input: string ): void {
-     this.memoService.onCreate(this.inputMemo);
-<<<<<<< HEAD
-     this.inputMemo = '';
-=======
->>>>>>> db3947d67c69e05500a00aa678b43c905a38eb0c
+    if (this.inputMemo ) {
+      this.memoService.onCreate(this.inputMemo);
+      this.inputMemo = '';
+    }
   }
 
 }
